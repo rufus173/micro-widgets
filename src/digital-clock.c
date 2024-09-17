afac65a2bc0607689c0cbb0c8f0579c64675948a
rufus173 <rufus09173@gmail.com>
2024-09-17 14:46:55 +0100

    added update loop

--- src/digital-clock.c
+++ src/digital-clock.c
@@ -8,15 +8,42 @@
 
 time_t now;
 struct tm *time_struct;
+struct label_container {
+	GtkWidget *date_label;
+	GtkWidget *time_label;
+};
 
 char *get_string_date();
 char *get_string_time();
 void update_time_struct();
+int update_loop(gpointer user_data);
 int create_window(GtkApplication *app,gpointer user_data){
 
 	//create window
 	GtkWidget *window;
 	window = gtk_application_window_new(app);
+	
+	//create a box to put widgets in
+	GtkWidget *box;
+	box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+	gtk_window_set_child(GTK_WINDOW(window), box);
+	//centre the box
+	gtk_widget_set_halign(box,GTK_ALIGN_CENTER);
+	gtk_widget_set_valign(box,GTK_ALIGN_CENTER);
+
+	//make the labels
+	struct label_container label_container;
+	label_container.date_label = gtk_label_new("Getting date...");
+	label_container.time_label = gtk_label_new("Getting time...");
+	
+	//fill box
+	gtk_box_append(GTK_BOX(box), label_container.date_label);
+	gtk_box_append(GTK_BOX(box), label_container.time_label);
+	
+	//set up timer
+	g_timeout_add(1000,update_loop,&label_container);
+
+	//present the window
 	gtk_window_present(GTK_WINDOW(window));
 }
 int main(int argc, char **argv){
@@ -28,6 +55,14 @@ int main(int argc, char **argv){
 	g_application_run(G_APPLICATION(app),argc,argv);
 	g_object_unref(app);
 	return 0;
+}
+int update_loop(gpointer user_data){
+	struct label_container *label_container;
+	label_container = user_data;
+	gchar *text = g_strdup_printf("%s",get_string_date);
+	gtk_label_set_label(GTK_LABEL(label_container->date_label),text);
+	g_free(text);
+	return G_SOURCE_CONTINUE;
 }
 void update_time_struct(){
 	time(&now);
