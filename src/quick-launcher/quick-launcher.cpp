15564249678878853e1c812618b9b6b317427529
rufus173 <derrickotron5000@gmail.com>
2024-09-20 23:10:32 +0100

    started on program class to store program info

--- src/quick-launcher/quick-launcher.cpp
+++ src/quick-launcher/quick-launcher.cpp
@@ -3,51 +3,72 @@
 #include <QGridLayout>
 #include <stdio.h>
 
-class debug{
+class debug_class{
 	private:
 		char function_name[1024] = "main";
 	public:
-		debug(const char *name){
+		debug_class(const char *name){
 			snprintf(function_name,1024,"%s",name);
 		}
-		void write_stderr(char *buffer){
-			fprintf(stderr,"ERROR in [%s]: %s\n",function_name,buffer);
+		void write_stderr(char *function_name, char *buffer){
+			fprintf(stderr,"ERROR in [%s]: %s\n",function_name, buffer);
 		}
-		void write_stdout(char *buffer){
-			printf("DEBUG: %s\n",buffer);
+		void write_stdout(char *function_name, char *buffer){
+			printf("DEBUG in [%s]: %s\n",function_name, buffer);
 		}
 		void operator<<(const char *buffer){
-			write_stdout((char *)buffer);
+			write_stdout(function_name, (char *)buffer);
 		}
 		void operator<(const char *buffer){
-			write_stderr((char *)buffer);
+			write_stderr(function_name, (char *)buffer);
 		}
 };
-static debug debug("main");
+class programs{
+private:
+	struct program_info{
+		char **app_name;
+		char **app_launch_command;
+	};
+	struct program_storage{
+		int program_count = 0;
+		struct program_info program_info;
+	};
+	struct program_storage program_storage;
+public:
+	programs(){
+		program_storage.program_count = 0;
+	}
+};
 
-void b1p();
-void b2p();
+static debug_class debug("main");
+
+void app_1();
+void app_2();
 
 int main(int argc, char **argv){
+	//set up the main app and window
 	QApplication app = QApplication(argc,argv);
 	QWidget *window = new QWidget();
 
-	QPushButton *button1 = new QPushButton("app1",window);
-	QPushButton *button2 = new QPushButton("app2",window);
-	QObject::connect(button1,&QPushButton::clicked,b1p);
-	QObject::connect(button2,&QPushButton::clicked,b2p);
+	//set up all the buttons
+	QPushButton *app_1_button = new QPushButton("app1",window);
+	QPushButton *app_2_button = new QPushButton("app2",window);
+	QObject::connect(app_1_button,&QPushButton::clicked,app_1);
+	QObject::connect(app_2_button,&QPushButton::clicked,app_2);
 
+	//add everything to a grid
 	QGridLayout *grid = new QGridLayout(window);
-	grid->addWidget(button1,0,0,1,2);
-	grid->addWidget(button2,0,2,1,1);
+	grid->addWidget(app_1_button,0,0,1,1);
+	grid->addWidget(app_2_button,0,1,1,1);
 
+	//present to user
 	window->show();
 	int status = app.exec();
 	return status;
 }
-void b1p(){
+void app_1(){
 	debug << "i am alive";
 }
-void b2p(){
+void app_2(){
 	debug < "i am dead";
 }
