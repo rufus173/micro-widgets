3ce1a2645935ad6b19c7ce88086909d73be1d0f5
rufus173 <rufus09173@gmail.com>
2024-09-24 19:08:12 +0100

    set up time and date for digital clock widget

--- src/micro-taskbar/digital_clock.cpp
+++ src/micro-taskbar/digital_clock.cpp
@@ -1,6 +1,9 @@
 //my headers
 #include "debug.h"
 
+//standard headers
+#include <time.h>
+
 //qt headers
 #include <QGridLayout>
 #include <QTimer>
@@ -8,6 +11,9 @@
 #include <QApplication>
 
 static debug_class debug("digital_clock");
+
+static char *date_string();
+static char *time_string();
 
 void build_digital_clock(QGridLayout *master_grid, int column){
 	//creating widgets
@@ -19,4 +25,29 @@ void build_digital_clock(QGridLayout *master_grid, int column){
 	widget_grid->addWidget(date,0,0,1,1);
 	widget_grid->addWidget(time,1,0,1,1);
 	master_grid->addLayout(widget_grid,0,column,1,1);
+
+	//create a time to update them
+	QTimer *digital_clock_update_timer = new QTimer();
+	QObject::connect(digital_clock_update_timer, &QTimer::timeout, [date,time]{
+			//update loop
+			date->setText(date_string());
+			time->setText(time_string());
+		});
+	digital_clock_update_timer->start(100);
 }
+static char *date_string(){
+	time_t time_now = time(NULL);
+	static char return_buffer[1024];
+	struct tm *time_struct;
+	time_struct = localtime(&time_now);
+	strftime(return_buffer,1024,"%d/%m/%y",time_struct);
+	return return_buffer;
+}	
+static char *time_string(){
+	time_t time_now = time(NULL);
+	static char return_buffer[1024];
+	struct tm *time_struct;
+	time_struct = localtime(&time_now);
+	strftime(return_buffer,1024,"%H:%M",time_struct);
+	return return_buffer;
+}	
