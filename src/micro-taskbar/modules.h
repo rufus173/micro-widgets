fa09ef0b1dd977d0bd4125bdcc1617c73d8963f7
rufus173 <rufus09173@gmail.com>
2024-09-26 14:42:40 +0100

    started on quick launcher widget

--- src/micro-taskbar/modules.h
+++ src/micro-taskbar/modules.h
@@ -4,4 +4,5 @@
 void build_digital_clock(QGridLayout *grid,int column);
 void build_battery_level(QGridLayout *grid,int column);
 void build_power_button(QGridLayout *grid,int column);
+void build_quick_launcher(QGridLayout *grid,int column);
 #endif
---
+++ src/micro-taskbar/quick_launcher.cpp
@@ -0,0 +1,16 @@
+//header yourself
+#include <QGridLayout>
+//header myself
+#include "debug.h"
+
+static debug_class debug = debug_class("quick_launcher");
+
+void build_quick_launcher(QGridLayout *master_grid, int column){
+	debug << "starting quick launcher construction";
+	QGridLayout *widget_grid = QGridLayout();
+
+	//procedural button creation
+	//i cant be bothered rn do this later
+
+	master_grid->addLayout(widget_grid,0,column,1,1);
+}
