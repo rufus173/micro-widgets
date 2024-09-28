361497abd0c088428bf3685ee93d8f3bdb0bcda5
rufus173 <rufus09173@gmail.com>
2024-09-28 17:12:32 +0100

    started on the tray

--- src/micro-taskbar/tray.cpp
+++ src/micro-taskbar/tray.cpp
@@ -1,3 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <wait.h>
+
+struct processes {
+	int count
+	pid_t *pid;
+};
+int new_process();
+
 int main_tray(){
+	//set up memory
+	struct processes proc;
+	proc.pid = malloc(sizeof(pid_t));
+	proc.count = 0;
+
+	//set up socket
+
+	//pid waiting
+	for (int i = 0; i < proc.count; i++){
+		waitpid(proc.pid[i],NULL,)
+	}
+	
+
 	return 0;
 }
