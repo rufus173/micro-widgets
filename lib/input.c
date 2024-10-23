e1cd47b9bd12ebcbeb1beb8f040d792e8f01a467
rufus173 <derrickotron5000@gmail.com>
2024-10-23 20:48:16 +0100

    broadened search for finding keyboard file

--- lib/input.c
+++ lib/input.c
@@ -21,6 +21,7 @@ struct keypress_info {
 	uint16_t keycode;
 	enum key_state state;
 };
+static char *search_in_dir(const char *search, const char *dir);
 
 // ------ private functions ---
 static char *to_lower_case(char *input){ //non destructive
@@ -29,6 +30,32 @@ static char *to_lower_case(char *input){ //non destructive
 		output[i] = tolower(input[i]);
 	}
 	return output;
+}
+static char *search_in_dir(const char *search_pattern, const char *dir_to_search){ //ls $dir_to_search | grep -i $search_pattern
+	char *result = NULL;
+	//ls
+	DIR *dir;
+	dir = opendir(dir_to_search);
+	if (dir == NULL){
+		fprintf(stderr,"could not open %s for reading\n",dir_to_search);
+		perror("opendir");
+		return NULL;
+	}
+	//for each thing in the dir
+	for (struct dirent *file_in_dir = readdir(dir) ; file_in_dir != NULL; file_in_dir = readdir(dir)){
+		//search for search_pattern in base_location directory
+		//grep -i
+		char *lower_case_filename = to_lower_case(file_in_dir->d_name);
+		if (strstr(lower_case_filename,search_pattern) != NULL){
+			int max_result_size = strlen(dir_to_search)+strlen(file_in_dir->d_name)+2; //dont forget about the slash and '\0'
+			result = malloc(sizeof(char)*max_result_size);
+			snprintf(result,max_result_size,"%s/%s",dir_to_search,file_in_dir->d_name);
+			break;
+		}
+		free(lower_case_filename);
+	}
+	closedir(dir);
+	return result;
 }
 
 // ------ public functions ----
@@ -41,27 +68,23 @@ int connect_input_fd(char *location){
 	}
 }
 char *get_keyboard_device_location(){
-	static char device_location[KEYBOARD_LOCATION_BUFFER_LENGTH] = {0};
 	const char *base_location = "/dev/input/by-id";
 	const char *search_pattern = "keyboard-event-kbd"; //use lower case
-	DIR *keyboard_id_dir;
 
-	// implement ls
-	keyboard_id_dir = opendir(base_location);
-	//for each thing in the dir
-	for (struct dirent *file_in_dir = readdir(keyboard_id_dir) ; file_in_dir != NULL; file_in_dir = readdir(keyboard_id_dir)){
-		//search for search_pattern in base_location directory
-		char *lower_case_filename = to_lower_case(file_in_dir->d_name);
-		if (strstr(lower_case_filename,search_pattern) != NULL){
-			//printf("match: %s;%s\n",file_in_dir->d_name,search_pattern);
-			snprintf(device_location,KEYBOARD_LOCATION_BUFFER_LENGTH,"%s/%s",base_location,file_in_dir->d_name);
-		}
-		free(lower_case_filename);
+	char *result;
+	result = search_in_dir(search_pattern,base_location);
+	if (result != NULL){
+		return result;
 	}
-	closedir(keyboard_id_dir);
 
-	//return complete location buffer
-	return device_location; //device_location[0] will be 0 if no result found
+	base_location = "/dev/input/by-path";
+	search_pattern = "event-kbd";
+	result = search_in_dir(search_pattern,base_location);
+	if (result != NULL){
+		return result;
+	}
+
+	return NULL; //could not find keyboard
 };
 int get_keypress(int input_fd, struct keypress_info *decoded_keypress){
 	// ----- get the input -----
