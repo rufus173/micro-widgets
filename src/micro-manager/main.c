198323b202ecd7b0e2a253f6b768ea5fa8a7d348
rufus173 <derrickotron5000@gmail.com>
2024-10-26 22:11:51 +0100

    added code for resizing windows

--- src/micro-manager/main.c
+++ src/micro-manager/main.c
@@ -37,30 +37,60 @@ int main(int argc, char **argv){
 	//mainloop
 	start.subwindow = None;
 	//the position of the cursor in relation to the window origin on window pickup
-	int x_diff = 0;
-	int y_diff = 0;
+	int x_window_cursor_diff = 0;
+	int y_window_cursor_diff = 0;
+	int start_cursor_x = 0;
+	int start_cursor_y = 0;
+	int start_window_x = 0;
+	int start_window_y = 0;
+	int mouse_button = 0;
 	for (;;){
 		XNextEvent(display,&event); //load the next event into its struct
 
 		//raise window
 		if (event.type == KeyPress && event.xkey.subwindow != None){
 			XRaiseWindow(display, event.xkey.subwindow);
+		
+		// --------------- start of click --------------
 		}else if (event.type == ButtonPress && event.xbutton.subwindow != None){
-			printf("window picked up\n");
+			mouse_button = event.xbutton.button;
 			XGetWindowAttributes(display,event.xbutton.subwindow, &attributes);//get selected window's attributes
 			XRaiseWindow(display, event.xkey.subwindow);
 			start = event.xbutton;
+			printf("====== start of click event ======\n");
+			switch(mouse_button){
+				case 1:
+					printf("event type: move\n");
+				break;
+				case 3:
+					printf("event type: resize\n");
+				break;
+			}
+			start_cursor_x = start.x_root; start_cursor_y = start.y_root;
+			start_cursor_y = start.x_root; start_window_y = start.y_root;
+			start_window_x = attributes.x;
+			start_window_y = attributes.y;
 			printf("start cursor pos: %d,%d\n",start.x_root, start.y_root);
 			printf("start window pos: %d, %d\n",attributes.x,attributes.y);
-		}else if (event.type == MotionNotify && start.subwindow != None){
+		//--------------- dragging the window ---------------
+		}else if (event.type == MotionNotify && start.subwindow != None && mouse_button == 1){
 			//printf("window moving\n");
-			x_diff = start.x_root - attributes.x; //so we drag the window from where we clicked it rather then the corner
-			y_diff = start.y_root - attributes.y;
+			x_window_cursor_diff = start.x_root - attributes.x; //so we drag the window from where we clicked it rather then the corner
+			y_window_cursor_diff = start.y_root - attributes.y;
 			printf("cursor (%d,%d)\n",event.xbutton.x_root,event.xbutton.y_root);
-			XMoveWindow(display, start.subwindow, event.xbutton.x_root-x_diff, event.xbutton.y_root-y_diff);
+			XMoveWindow(display, start.subwindow, start_cursor_x-x_window_cursor_diff, start_cursor_y-y_window_cursor_diff);
+		//-------------- resizing the window -----------------
+		}else if(event.type == MotionNotify && start.subwindow != None && mouse_button == 3){
+			unsigned int new_width = clamp_min(event.xbutton.x_root - start_window_x,1);
+			unsigned int new_height = clamp_min(event.xbutton.y_root - start_window_y,1);
+			printf("resize to %dx%d\n",new_width,new_height);
+			XResizeWindow(display, start.subwindow, new_width, new_height);
+			
+		//-------------- button released --------------
 		}else if (event.type == ButtonRelease){
-			printf("window dropped\n");
+			printf("====== end of click event ======\n");
 			start.subwindow = None;
+			mouse_button = 0;
 		}
 	}
 
