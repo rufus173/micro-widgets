7a86ac51d1e484c6d5e0b3f264d79419a8bf5d19
rufus173 <derrickotron5000@gmail.com>
2025-06-03 23:25:36 +0100

    implemented a grid layout

--- dsply/src/main.rs
+++ dsply/src/main.rs
@@ -1,19 +1,30 @@
 use glib::clone;
 use gtk4::glib;
 use gtk4::prelude::*;
+use std::path::Path;
+use gtk4::gdk_pixbuf::Pixbuf;
 
 pub struct Images {
-	pixbufs: Vec<gtk4::gdk_pixbuf::Pixbuf>
+	pixbufs: Vec<gtk4::gdk_pixbuf::Pixbuf>,
 }
 impl Images {
-	fn new(files: Vec<String>) -> Images{
-		let images = Images {
-			pixbufs: Vec::new()
+	fn new(files: Vec<String>) -> Option<Images>{
+		let mut images = Images {
+			pixbufs: Vec::new(),
 		};
 		for file in files{
 			println!("{}",file);
+			let path = Path::new(&file);
+			if !path.exists(){
+				println!("Error: {} does not exist",file);
+				continue;
+			}
+			images.pixbufs.push(match Pixbuf::from_file(&file){
+				Err(e) => {println!("{}",e); continue;},
+				Ok(pixbuf) => pixbuf,
+			});
 		}
-		images
+		Some(images)
 	}
 }
 
@@ -27,7 +38,8 @@ fn main() {
 
 fn on_activate(application: &gtk4::Application){
 	//====== load the images ======
-	let images = Images::new(std::env::args().collect());
+	let file_list: Vec<String> = std::env::args().collect();
+	let images = Images::new(file_list[1..].to_vec());
 	//====== build the gui ======
 	let window = gtk4::ApplicationWindow::builder()
 		.application(application)
@@ -36,12 +48,15 @@ fn on_activate(application: &gtk4::Application){
 		.default_width(500)
 		.default_height(500)
 		.build();
+	let grid = gtk4::Grid::new();
+	window.set_child(Some(&grid));
 	let close_button = gtk4::Button::with_label("Close");
 	close_button.connect_clicked(
 		clone!(#[strong] window, move |_|{
 			window.close();
 		})
 	);
-	window.set_child(Some(&close_button));
+	GridExt::attach(&grid,&close_button,0,0,1,1);
+	let image_display = gtk4::Image::new();
 	window.present();
 }
